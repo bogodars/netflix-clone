6c6d260cc1e68f388c6ee64a51e7ff2a50a9c492
Bogodar Shmyglyk <bogodar@gmail.com>
2021-02-14 03:50:28 +0200

    Nav ready, banner in progress

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Banner from './Banner';
 import './HomeScreen.css';
 import Nav from './Nav';
 
 const HomeScreen = () => {
   return (
     <div className='homeScreen'>
-      {' '}
       <Nav />
+
+      <Banner />
     </div>
   );
 };
--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Nav.css';
+
 const Nav = () => {
+  const [show, handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', transitionNavBar);
+    return () => window.removeEventListener('scroll', transitionNavBar);
+  }, []);
+
   return (
-    <div className='nav nav__black'>
+    <div className={`nav ${show && 'nav__black'}`}>
       <div className='nav__content'>
         <img
           className='nav__logo'
